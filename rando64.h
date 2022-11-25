5784cd68c20520379056b41ac6c25cdb1662f60e
patgiri <89099315+patgiri@users.noreply.github.com>
2022-11-26 00:51:28 +0530

    Update rando64.h

--- rando64.h
+++ rando64.h
@@ -1,6 +1,5 @@
 #include <time.h>
 #include "murmur.h"
-#define size 32
 
 unsigned long int genRando()
 {
@@ -8,7 +7,7 @@ unsigned long int genRando()
 	int *binary=(int *)malloc(64*sizeof(int));
 	char key[1024],c;
 	int m=64;
-	int p,q,j,k=1;
+	int p,q;
 	unsigned long int rand=0;
 	q=clock()^15485867;
 	while(i<m)
@@ -16,11 +15,11 @@ unsigned long int genRando()
 		p=clock();
 		sprintf(key,"%d",p);
 		for(j=0;key[j]!='\0';j++);
-		q=q^p;
 		d=murmur2(key,j,q);
+		d=q;
 		d=d&1;
 		binary[i]=d;
-		rand+=d*(k<<i);
+		rand+=d*(1L<<i);
 		i++;
 		
 	}
